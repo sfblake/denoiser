1ea0640700305f2e3701dac3fff2aa188df9161c
sfblake <samuelfblake@gmail.com>
2022-07-21 22:39:36 +0100

    basic predict function

---
+++ denoiser/predict.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+import logging
+import numpy as np
+import os
+from scipy.io import wavfile
+import tensorflow as tf
+
+
+def process_file(path_to_file: str, model_bitrate: int, model: tf.keras.Model):
+    """
+
+    :param path_to_file:
+    :return:
+    """
+    logging.info("Reading file {}".format(path_to_file))
+    bitrate, data = wavfile.read(os.path.join(path_to_file))
+    if bitrate != model_bitrate:
+        raise ValueError("File bitrate {} does not match model bitrate {}"
+                         .format(bitrate, model_bitrate))
+    track_length = data.shape[0]
+    sample_length = model.inputs[0].shape[1]
+
+    # Pad to get complete samples
+    num_samples = int(np.ceil(track_length / sample_length))
+    extra_steps = int(num_samples * sample_length - track_length)
+    front_pad = np.floor(extra_steps / 2).astype(int)
+    back_pad = np.ceil(extra_steps / 2).astype(int)
+    data_padded = np.pad(data, pad_width=((front_pad, back_pad), (0, 0)), mode='edge')
+    logging.info("File length {:.0f}s split into {} samples"
+                 .format(track_length/bitrate, num_samples))
+
+    predict_start = datetime.now()
+    preds = model.predict(
+        data_padded.reshape(num_samples, sample_length, 2)
+    )
+    preds = preds.reshape(-1)[front_pad:-back_pad]
+    logging.info("Finished predicting in {}s"
+                 .format((datetime.now() - predict_start).seconds))
